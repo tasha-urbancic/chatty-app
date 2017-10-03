eb910a67196c575c7639066ff900ba835dea016c
tasha-urbancic <tasha.urbancic@gmail.com>
2017-10-03 03:21:57 +0000

    added components JSX files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
 
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
+var name = "Chatty";
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">{name}</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
